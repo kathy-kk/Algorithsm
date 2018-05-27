c55173118742c83d70b6caa8425b2f52e318f469
kathy y <meimeiy1121@gmail.com>
2018-05-27 19:58:02 +1000

    quick union with size track making sure that smaller tree connect to larger tree

---
+++ quickUnion.js
@@ -0,0 +1,26 @@
+const quickUnionUF = (n) => {
+   const id = new Array(n).map((value, index) => {
+     return index
+   });
+   const sz = new Array(n).fill(1);
+
+   const root = (i) => {
+     while(i != id[i]) i = id[i];
+     return i
+   } 
+   const connected = (p, q) => {
+      return root(p) == root(q);
+   }
+   const union = (p, q) => {
+      const i = root(p);
+      const j = root(q);
+      if (i == j) return;
+      if (sz[i] < sz[j]){
+        id[i] = j;
+        sz[j] += sz[i]
+      }else{
+        id[j] = i;
+        sz[i] += sz[j];
+      }
+   }
+}
